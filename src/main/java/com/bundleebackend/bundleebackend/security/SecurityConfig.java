96bdf67e36d7b036b96c20205effc866b38177a2
Jacob Knock <jakeknocker74@gmail.com>
2023-09-16 12:29:25 -0400

    next

--- src/main/java/com/bundleebackend/bundleebackend/security/SecurityConfig.java
+++ src/main/java/com/bundleebackend/bundleebackend/security/SecurityConfig.java
@@ -4,21 +4,11 @@ import com.bundleebackend.bundleebackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.sql.DataSource;
 
@@ -26,8 +16,6 @@ import javax.sql.DataSource;
 public class SecurityConfig {
     @Autowired
     private UserRepository userRepository;
-//    @Autowired
-//    private JwtFilter jwtFilter;
 
     @Bean
     public UserDetailsManager userDetailsManager(DataSource dataSource) {
@@ -35,30 +23,15 @@ public class SecurityConfig {
         return userDetailsManager;
     }
 
-//    @Bean
-//    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
-//        return authConfig.getAuthenticationManager();
-//    }
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth ->
-                                auth.requestMatchers("/api/metacritic/games").permitAll()
-                                        .requestMatchers("/api/auth/login").permitAll()
-                                        .requestMatchers("/users").permitAll()
-                                        .requestMatchers("users/**").permitAll()
-                                        .requestMatchers("/").permitAll()
-                                        .requestMatchers("/**").permitAll()
+                                auth.requestMatchers("/**").permitAll()
                 )
                         .sessionManagement(sess ->
                                 sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                         );
         return http.build();
     }
-
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
 }
--- src/main/java/com/bundleebackend/bundleebackend/util/AuthUtil.java
+++ src/main/java/com/bundleebackend/bundleebackend/util/AuthUtil.java
@@ -6,7 +6,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @Component
